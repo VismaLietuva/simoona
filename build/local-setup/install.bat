69e928141513b8f98de7ecb8ec0b361a134b4598
Aleksandras <aleksandrr@gmail.com>
2019-05-29 14:06:25 +0300

    Added local setup build script

---
+++ build/local-setup/install.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+set connectionString=
+set /p connectionString=Please provide ConnectionString (default: Data Source=localhost\SQLEXPRESS;Integrated Security=True;Connect Timeout=60; MultipleActiveResultSets=True;): || set connectionString=Data Source=localhost\SQLEXPRESS;Integrated Security=True;Connect Timeout=60; MultipleActiveResultSets=True;
+
+powershell -ExecutionPolicy ByPass -File build.ps1 -script build.cake -target="Default" -connectionString="%connectionString%"
+PAUSE
