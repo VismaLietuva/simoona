1d982e5bfb15c3bf6d1f7af28f62babf2fd74c01
Zilvinas Mikulis <zilvinas.mikulis@visma.com>
2019-07-31 17:06:23 +0300

    Refactored long running notifications from Post and Comment controllers into background workers

---
+++ src/api/Infrastructure/Shrooms.Infrastructure/FireAndForget/AsyncRunner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Hosting;
+using Autofac;
+using Autofac.Core.Lifetime;
+using Autofac.Features.OwnedInstances;
+
+namespace Shrooms.Infrastructure.FireAndForget
+{
+    public class AsyncRunner : IAsyncRunner
+    {
+        public ILifetimeScope LifetimeScope { get; set; }
+
+        public AsyncRunner(ILifetimeScope lifetimeScope)
+        {
+            LifetimeScope = lifetimeScope;
+        }
+
+        public void Run<T>(Action<T> action, string tenantName)
+        {
+            HostingEnvironment.QueueBackgroundWorkItem(ct =>
+            {
+                using (var container = LifetimeScope.BeginLifetimeScope(MatchingScopeLifetimeTags.RequestLifetimeScopeTag, (builder) =>
+                 {
+                     builder.RegisterInstance(new TenantNameContainer(tenantName)).As<ITenantNameContainer>().SingleInstance();
+                 }))
+                {
+                    var service = container.Resolve<T>();
+                    action(service);
+                }
+            });
+        }
+    }
+
+    public interface IAsyncRunner
+    {
+        void Run<T>(Action<T> action, string tenantName);
+    }
+}
--- src/api/Main/BuisnessLayer/Shrooms.Domain/Services/Email/Posting/CommentNotificationService.cs
+++ src/api/Main/BuisnessLayer/Shrooms.Domain/Services/Email/Posting/CommentNotificationService.cs
@@ -2,12 +2,15 @@
 using System.Data.Entity;
 using System.Linq;
 using Shrooms.Constants;
+using Shrooms.Constants.ErrorCodes;
 using Shrooms.DataLayer.DAL;
 using Shrooms.DataTransferObjects.EmailTemplateViewModels;
 using Shrooms.DataTransferObjects.Models.Emails;
+using Shrooms.DataTransferObjects.Models.Wall.Posts.Comments;
 using Shrooms.Domain.Helpers;
 using Shrooms.Domain.Services.Organizations;
 using Shrooms.Domain.Services.UserService;
+using Shrooms.DomainExceptions.Exceptions;
 using Shrooms.EntityModels.Models;
 using Shrooms.EntityModels.Models.Multiwall;
 using Shrooms.Infrastructure.Configuration;
@@ -28,6 +31,8 @@ namespace Shrooms.Domain.Services.Email.Posting
 
         private readonly IDbSet<EntityModels.Models.Events.Event> _eventsDbSet;
         private readonly IDbSet<Project> _projectsDbSet;
+        private readonly IDbSet<Comment> _commentsDbSet;
+
 
         public CommentNotificationService(
             IUnitOfWork2 uow,
@@ -47,6 +52,7 @@ namespace Shrooms.Domain.Services.Email.Posting
 
             _eventsDbSet = uow.GetDbSet<EntityModels.Models.Events.Event>();
             _projectsDbSet = uow.GetDbSet<Project>();
+            _commentsDbSet = uow.GetDbSet<Comment>();
         }
 
         public void NotifyAboutNewComment(Comment comment, ApplicationUser commentCreator)
@@ -62,7 +68,43 @@ namespace Shrooms.Domain.Services.Email.Posting
 
             if (destinationEmails.Count > 0)
             {
-                var postLink = GetPostLink(comment.Post.Wall, organization.ShortName, comment.Post);
+                var postLink = GetPostLink(comment.Post.Wall.Type,comment.Post.WallId, organization.ShortName, comment.Post.Id);
+                var authorPictureUrl = _appSettings.PictureUrl(organization.ShortName, commentCreator.PictureId);
+                var userNotificationSettingsUrl = _appSettings.UserNotificationSettingsUrl(organization.ShortName);
+                var subject = string.Format(Templates.NewPostCommentEmailSubject, CutMessage(comment.Post.MessageBody), commentCreator.FullName);
+                var body = _markdownConverter.ConvertToHtml(comment.MessageBody);
+
+                var emailTemplateViewModel = new NewCommentEmailTemplateViewModel(
+                    string.Format(Constants.BusinessLayer.Templates.PostCommentTitle, CutMessage(comment.Post.MessageBody)),
+                    authorPictureUrl,
+                    commentCreator.FullName,
+                    postLink,
+                    body,
+                    userNotificationSettingsUrl,
+                    Constants.BusinessLayer.Templates.DefautlActionButtonTitle);
+
+                var content = _mailTemplate.Generate(emailTemplateViewModel, EmailTemplateCacheKeys.NewPostComment);
+                var emailData = new EmailDto(destinationEmails, subject, content);
+                _mailingService.SendEmail(emailData);
+            }
+        }
+
+        public void NotifyAboutNewComment(CommentCreatedDTO commentDto)
+        {
+            var commentCreator = _userService.GetApplicationUser(commentDto.CommentCreator);
+            var organization = _organizationService.GetOrganizationById(commentCreator.OrganizationId);
+
+            var destinationEmails = _userService.GetPostCommentersEmails(commentCreator.Email, commentDto.PostId);
+            var postAuthorEmail = (commentDto.CommentCreator == commentDto.PostCreator) ? null : _userService.GetPostAuthorEmail(commentDto.PostCreator);
+            if (postAuthorEmail != null && destinationEmails.Contains(postAuthorEmail) == false)
+            {
+                destinationEmails.Add(postAuthorEmail);
+            }
+
+            if (destinationEmails.Count > 0)
+            {
+                var comment = LoadComment(commentDto.CommentId);
+                var postLink = GetPostLink(commentDto.WallType,commentDto.WallId, organization.ShortName, commentDto.PostId);
                 var authorPictureUrl = _appSettings.PictureUrl(organization.ShortName, commentCreator.PictureId);
                 var userNotificationSettingsUrl = _appSettings.UserNotificationSettingsUrl(organization.ShortName);
                 var subject = string.Format(Templates.NewPostCommentEmailSubject, CutMessage(comment.Post.MessageBody), commentCreator.FullName);
@@ -99,25 +141,39 @@ namespace Shrooms.Domain.Services.Email.Posting
             return value;
         }
 
-        private string GetPostLink(EntityModels.Models.Multiwall.Wall wall, string orgName, Post post)
+        private string GetPostLink(WallType wallType,int wallId, string orgName, int postId)
         {
-            switch (wall.Type)
+            switch (wallType)
             {
                 case WallType.Events:
                     var eventId = _eventsDbSet
-                        .Where(x => x.WallId == wall.Id)
+                        .Where(x => x.WallId == wallId)
                         .Select(x => x.Id)
                         .First();
                     return _appSettings.EventUrl(orgName, eventId.ToString());
                 case WallType.Project:
                     var projectId = _projectsDbSet
-                        .Where(x => x.WallId == wall.Id)
+                        .Where(x => x.WallId == wallId)
                         .Select(x => x.Id)
                         .First();
                     return _appSettings.ProjectUrl(orgName, projectId.ToString());
                 default:
-                    return _appSettings.WallPostUrl(orgName, post.Id);
+                    return _appSettings.WallPostUrl(orgName, postId);
             }
+        }
+
+        private Comment LoadComment(int commentId)
+        {
+            var comment = _commentsDbSet
+                            .Include(x => x.Post)
+                            .FirstOrDefault(c =>
+                                c.Id == commentId);
+
+            if (comment == null)
+            {
+                throw new ValidationException(ErrorCodes.ContentDoesNotExist, $"Comment {commentId} does not exist");
+            }
+            return comment;
         }
     }
 }
