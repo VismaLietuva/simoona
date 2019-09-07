a39c067226a209bed354666ef3674be53a753930
Aleksandras Tabler <aleksandras.tabler@visma.com>
2019-09-07 21:00:54 +0300

    Regenerated "postwatchers" migrations

---
+++ src/api/Main/DataLayer/Shrooms.DataLayer/Migrations/201909071758421_PostWatchers.cs
@@ -0,0 +1,32 @@
+namespace Shrooms.DataLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PostWatchers : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                    "dbo.PostWatchers",
+                    c => new
+                    {
+                        PostId = c.Int(nullable: false),
+                        UserId = c.Guid(nullable: false),
+                    })
+                .PrimaryKey(pk => new { pk.PostId, pk.UserId }, "PK_PostWatchers")
+                .ForeignKey("dbo.Posts", t => t.PostId, cascadeDelete: true);
+
+            Sql("insert into dbo.PostWatchers (PostId,UserId) " +
+                "select distinct postid,authorid from dbo.Comments " +
+                " union " +
+                "select distinct id, AuthorId from dbo.Posts");
+
+        }
+
+        public override void Down()
+        {
+            DropTable("dbo.PostWatchers");
+        }
+    }
+}
