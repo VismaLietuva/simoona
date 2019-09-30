e034cc54c4316aba48ad9f17a6b4720c01a3b7b3
Edvinas Sadeckas <edvinas.sadeckas@visma.com>
2019-09-30 14:55:35 +0300

    disable DOM loading for not shown elements

--- src/webapp/src/client/app/wall/wall.controller.js
+++ src/webapp/src/client/app/wall/wall.controller.js
@@ -3,24 +3,38 @@
 
     angular
         .module('simoonaApp.Wall')
+        .constant('minDisplaySizes', {
+            rightSidebar: 992
+        })
         .controller('wallController', wallController);
 
     wallController.$inject = [
         '$rootScope',
+        '$window',
+        '$timeout',
+        'minDisplaySizes',
         'wallRepository'
     ];
 
-    function wallController($rootScope, wallRepository) {
+    function wallController($rootScope, $window, $timeout, minDisplaySizes, wallRepository) {
         /*jshint validthis: true */
         var vm = this;
 
         $rootScope.pageTitle = 'wall.wallTitle';
 
         //init
+        vm.rightBarMinSize = minDisplaySizes.rightSidebar;
+        vm.windowWidth = $window.innerWidth;
         vm.widgetsInfo = {};
         wallRepository.getWidgetsInfo()
-            .then(function(widgetsInfo) { 
+            .then(function (widgetsInfo) {
                 vm.widgetsInfo = widgetsInfo;
             });
+
+        $window.onresize = function () {
+            $timeout(function () {
+                vm.windowWidth = $window.innerWidth;
+            });
+        };
     }
 }());
