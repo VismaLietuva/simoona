f9cad9ad9ef8b9bc0340a0bcb8fbf928c40eae3e
Jonas Ložys <jonas.lozys@visma.com>
2019-10-03 16:19:21 +0300

    Add Lottery and LotteryParticipant entities.

---
+++ src/api/Main/DataLayer/Shrooms.EntityModels/Models/Lottery/ImageBaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shrooms.EntityModels.Models.Lottery
+{
+    public abstract class ImageBaseModel : BaseModelWithOrg
+    {
+        public virtual ImagesCollection Images { get; set; }
+    }
+}
---
+++ src/api/Main/DataLayer/Shrooms.EntityModels/Models/Lottery/Lottery.cs
@@ -0,0 +1,26 @@
+﻿using Shrooms.Constants.EntityValidationValues;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shrooms.EntityModels.Models.Lottery
+{
+    public class Lottery : ImageBaseModel
+    {
+        public string Title { get; set; }
+
+        [StringLength(ValidationConstants.MaxPostMessageBodyLength)]
+        public string Description { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int Status { get; set; }
+
+        public int EntryFee { get; set; }
+
+
+    }
+}
