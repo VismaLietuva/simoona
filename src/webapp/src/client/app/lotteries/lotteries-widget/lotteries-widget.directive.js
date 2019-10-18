55d5d9be6f8d47115c73083930aaf7b1658467e8
Kristupas Lunskas <kristupas.lunskas@visma.com>
2019-10-18 11:29:11 +0300

    Remove lotttery carousel attribute tag and  fix timespan filter

--- src/webapp/src/client/app/lotteries/lotteries-widget/lotteries-widget.directive.js
+++ src/webapp/src/client/app/lotteries/lotteries-widget/lotteries-widget.directive.js
@@ -50,68 +50,6 @@
             if (!input) {
                 return '';
             }
-        var timestamp = new Date(input);
-
-    
-        if (timestamp == 'Invalid Date') {
-            return '(invalid date)';
-        }
-
-        var now = new Date();
-        var nowUtc = new Date( now.getTime() + (now.getTimezoneOffset() * 60 * 1000));
-
-        var year = timestamp.getFullYear();
-
-        if (year < 1970) {
-            return (now.getFullYear() - year) + ' years ago';
-        }
-
-        var seconds = ((timestamp - nowUtc) * .001) >> 0;
-
-        if (seconds < 0) {
-            seconds = Math.abs(seconds);
-        }
-
-        var minutes = seconds / 60;
-        var hours = minutes / 60;
-        var days = hours / 24;
-        var years = days / 365;
-
-        if (seconds < 10) {
-            return 'a few seconds';
-        }
-        else if (seconds < 60) {
-            return Math.round(seconds) + ' seconds';
-        }
-        else if (minutes < 1.5) {
-            return 'a minute';
-        }
-        else if (minutes < 60) {
-            return Math.round(minutes) + ' minutes';
-        }
-        else if (hours < 1.5) {
-            return 'an hour';
-        }
-        else if (hours < 24) {
-            return Math.round(hours) + ' hours';
-        }
-        else if (hours < 42) {
-                return 'tomorrow';
-        }
-        else if (days < 30) {
-            return Math.round(days) + ' days';
-        }
-        else if (days < 45) {
-            return 'a month';
-        }
-        else if (days < 365) {
-            return Math.round(days / 30) + ' months';
-        }
-        else if (years < 1.5) {
-            return 'a year';
-        }
-        else {
-            return Math.round(years) + ' years' ;
-        }
+            return moment.utc(input).fromNow();
         }
 })();
