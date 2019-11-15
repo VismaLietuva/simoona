e63a69d72405a4d0a5b64551624920d5796e0532
Kristupas Lunskas <kristupas.lunskas@visma.com>
2019-11-15 15:06:34 +0200

    Fix style

--- src/webapp/src/client/app/events/create-edit/create-edit.controller.js
+++ src/webapp/src/client/app/events/create-edit/create-edit.controller.js
@@ -229,9 +229,8 @@
             }
         }
 
-        function saveEvent(method, NewImage) {
-            
-            if (NewImage.length) {
+        function saveEvent(method, newImage) {
+            if (newImage.length) {
                 var eventImageBlob = dataHandler.dataURItoBlob(vm.eventCroppedImage[0], vm.eventImage[0].type);
 
                 eventImageBlob.lastModifiedDate = new Date();
