8f101289c62105205bd8d0acb8592f6ba11e4187
Aleksandras <aleksandrr@gmail.com>
2021-04-13 19:03:15 +0300

    Added a fallback logic for recurring events timezone calculation (moved constant to a different place)

--- src/api/Shrooms.DataLayer.EntityModels/Models/Events/Event.cs
+++ src/api/Shrooms.DataLayer.EntityModels/Models/Events/Event.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Newtonsoft.Json;
+using Shrooms.Contracts.Constants;
 using Shrooms.Contracts.DataTransferObjects;
 using Shrooms.DataLayer.EntityModels.Models.Multiwall;
 
@@ -76,9 +77,9 @@ namespace Shrooms.DataLayer.EntityModels.Models.Events
         }
 
         private DateTime GetLocalDateFromUtcDate(DateTime utcDateTime) =>
-            TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, TimeZoneInfo.FindSystemTimeZoneById(ResponsibleUser == null ? EventsConstants.DefaultTimeZone : ResponsibleUser.TimeZone));
+            TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, TimeZoneInfo.FindSystemTimeZoneById(ResponsibleUser == null ? DataLayerConstants.DefaultTimeZone : ResponsibleUser.TimeZone));
 
         private DateTime GetUtcDateFromLocalDate(DateTime localDateTime) =>
-            TimeZoneInfo.ConvertTimeToUtc(localDateTime, TimeZoneInfo.FindSystemTimeZoneById(ResponsibleUser == null ? EventsConstants.DefaultTimeZone : ResponsibleUser.TimeZone));
+            TimeZoneInfo.ConvertTimeToUtc(localDateTime, TimeZoneInfo.FindSystemTimeZoneById(ResponsibleUser == null ? DataLayerConstants.DefaultTimeZone : ResponsibleUser.TimeZone));
     }
 }
--- src/api/Shrooms.DataLayer.EntityModels/Models/Events/EventsConstants.cs
+++
@@ -1,7 +0,0 @@
-namespace Shrooms.DataLayer.EntityModels.Models.Events
-{
-    public static class EventsConstants
-    {
-        public const string DefaultTimeZone = "FLE Standard Time";
-    }
-}
