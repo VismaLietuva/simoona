3cfa602ca174074a1571d41e14674f6cd88c17ee
Justinas Ruslys <justinas.ruslys@visma.com>
2021-12-01 14:13:20 +0200

    Added enum for Like types, updated and created new dtos

---
+++ src/api/Shrooms.Contracts/DataTransferObjects/Wall/Likes/AddLikeDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Shrooms.Contracts.Enums;
+
+namespace Shrooms.Contracts.DataTransferObjects.Wall.Likes
+{
+    public class AddLikeDto
+    {
+        [Required]
+        public int Id { get; set; }
+        
+        [Required]
+        [EnumDataType(typeof(LikeTypeEnum))]
+        public LikeTypeEnum LikeType { get; set; }
+    }
+}
---
+++ src/api/Shrooms.Contracts/DataTransferObjects/Wall/Likes/LikeDto.cs
@@ -0,0 +1,15 @@
+﻿using Shrooms.Contracts.Enums;
+
+namespace Shrooms.Contracts.DataTransferObjects.Wall.Likes
+{
+    public class LikeDto
+    {
+        public string UserId { get; set; }
+
+        public string FullName { get; set; }
+
+        public string PictureId { get; set; }
+
+        public LikeTypeEnum LikeType { get; set; }
+    }
+}
