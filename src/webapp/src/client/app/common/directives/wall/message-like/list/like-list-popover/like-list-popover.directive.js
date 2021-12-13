03c4762ab840560bf10196b4a8e176ed08f8bf52
Justinas Ruslys <justinas.ruslys@visma.com>
2021-12-13 15:44:41 +0200

    Modified popover and added modal to view user likes

--- src/webapp/src/client/app/common/directives/wall/message-like/list/like-list-popover/like-list-popover.directive.js
+++ src/webapp/src/client/app/common/directives/wall/message-like/list/like-list-popover/like-list-popover.directive.js
@@ -3,15 +3,19 @@
 
     angular
         .module('simoonaApp.Common')
-        .directive('aceMessageLikeListPopover', messageLikeListPopover);
+        .directive('aceMessageLikeListPopover', messageLikeListPopover)
+        .constant('popoverLikesCount', 15);
 
     messageLikeListPopover.$inject = [
         '$compile',
         '$window',
-        '$templateCache'
+        '$templateCache',
+        '$uibModal',
+        'likeTypes',
+        'popoverLikesCount'
     ];
 
-    function messageLikeListPopover($compile, $window, $templateCache) {
+    function messageLikeListPopover($compile, $window, $templateCache, $uibModal, likeTypes, popoverLikesCount) {
         var lastLikePopoverElement;
         var directive = {
             restrict: 'A',
@@ -19,13 +23,34 @@
             templateUrl: 'app/common/directives/wall/message-like/list/like-list-popover/like-list-popover.html',
             scope: {
                 likes: '=',
-                popoverPlacement: '@'
+                modalLikes: '=',
+                popoverPlacement: '@',
+                popoverTitle: '@',
+                popoverEmoji: '@',
+                allLikeTypes: '@',
+                modalLikesTab: '@'
             },
             link: linkFunc
         };
         return directive;
 
         function linkFunc(scope, element) {
+            element.bind('click', function () {
+                $uibModal.open({
+                    templateUrl: 'app/common/directives/wall/message-like/list/like-list-popover/like-list-popover-modal.html',
+                    windowTopClass: 'popover-likes-modal',
+                    controller: popoverModalController,
+                    controllerAs: 'vm',
+                    resolve : {
+                        popoverScope: scope,
+                    }
+                });
+            });
+
+            scope.likeTypes = likeTypes;
+            scope.popoverLikesCount = popoverLikesCount;
+            scope.hiddenUsersCount = (scope.likes.length - 1) - popoverLikesCount;
+
             var html = $templateCache.get('messageLikeListPopoverTemplate.html');
             var popoverContent = $compile(html)(scope);
 
@@ -33,23 +58,61 @@
                 html: true,
                 content: popoverContent,
                 placement: scope.popoverPlacement,
-                trigger: 'manual'
+                trigger: 'hover',
             }).click(function (event) {
-                hideLastLikePopover(this);
-                lastLikePopoverElement = element;
-                angular.element(this).popover('toggle');
+                angular.element(this).popover('hide');
                 event.stopPropagation();
             });
+        }
+    }
 
-            $window.onclick = function () {
-                hideLastLikePopover();
-            };
+    popoverModalController.$inject = [
+        '$uibModalInstance',
+        'popoverScope',
+        'likeTypes'
+    ];
 
-            function hideLastLikePopover(newElement) {
-                if (lastLikePopoverElement && lastLikePopoverElement !== newElement) {
-                    angular.element(lastLikePopoverElement).popover('hide');
-                }
+    function popoverModalController($uibModalInstance, popoverScope, likeTypes) {
+
+        var vm = this;
+
+        vm.modalLikes = popoverScope.modalLikes;
+        vm.currentTab = popoverScope.modalLikesTab;
+        vm.likeTypes = likeTypes;
+        vm.showAllLikesTab = true;
+
+        vm.switchTab = switchTab;
+        vm.closeModal = closeModal;
+
+        init();
+
+        //////////
+
+        function init() {
+            if(!canShowAllLikesTab()) {
+                vm.showAllLikesTab = false;
             }
+
+            // By default show all likes
+            if(vm.currentTab === undefined) {
+                vm.currentTab = vm.likeTypes.length;
+            }
+        }
+
+        function switchTab(tabIndex) {
+            vm.currentTab = tabIndex;
+        }
+
+        function canShowAllLikesTab() {
+            var receivedLikes = vm.modalLikes.filter(function(likes) {
+                return likes.length === 0;
+            })
+
+            return receivedLikes.length !== vm.likeTypes.length - 1;
+        }
+
+        function closeModal() {
+            $uibModalInstance.close();
         }
     }
 }());
