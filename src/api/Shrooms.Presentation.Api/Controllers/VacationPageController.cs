a58a1ee92008d78d760d4e8d7da85436e9daf495
Justinas Ruslys <justinas.ruslys@visma.com>
2021-12-16 12:59:19 +0200

    Added VacationPageController and implemented Get and Edit end-points

---
+++ src/api/Shrooms.Presentation.Api/Controllers/VacationPageController.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using AutoMapper;
+using Shrooms.Contracts.Constants;
+using Shrooms.Contracts.DataTransferObjects.VacationPages;
+using Shrooms.Domain.Services.VacationPages;
+using Shrooms.Presentation.Api.Filters;
+using Shrooms.Presentation.WebViewModels.Models.VacationPage;
+
+namespace Shrooms.Presentation.Api.Controllers
+{
+    [Authorize]
+    [RoutePrefix("VacationPage")]
+    public class VacationPageController : BaseController
+    {
+        private readonly IVacationPageService _vacationPageService;
+        private readonly IMapper _mapper;
+
+        public VacationPageController(IVacationPageService vacationPageService, IMapper mapper)
+        {
+            _vacationPageService = vacationPageService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public async Task<IHttpActionResult> GetVacationPage()
+        {
+            var vacationPageDto = await _vacationPageService.GetVacationPage(GetOrganizationId());
+
+            if (vacationPageDto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<VacationPageDto, VacationPageViewModel>(vacationPageDto));
+        }
+
+        [HttpPut]
+        [Route("Edit")]
+        [PermissionAuthorize(Permission = AdministrationPermissions.Vacation)]
+        public async Task<IHttpActionResult> EditVacationPage(VacationPageViewModel vacationPageViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var userAndOrg = GetUserAndOrganization();
+            var vacationPageDto = _mapper.Map<VacationPageViewModel, VacationPageDto>(vacationPageViewModel);
+
+            await _vacationPageService.EditVacationPage(userAndOrg, vacationPageDto);
+
+            return Ok();
+        }
+    }
+}
