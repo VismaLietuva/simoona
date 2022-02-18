fa8269bcde2775d438234da32cfc684097bca6e4
INTERNAL\justinas.ruslys <justinas.ruslys@visma.com>
2022-02-18 10:44:56 +0200

    changed enum order to match old frontend enum

--- src/api/Shrooms.Contracts.DataTransferObjects/Models/Wall/WallsListFilter.cs
+++ src/api/Shrooms.Contracts.DataTransferObjects/Models/Wall/WallsListFilter.cs
@@ -3,9 +3,9 @@
     public enum WallsListFilter
     {
         None,
-        All,
-        NotHiddenFromAllWalls,
         Followed,
+        NotHiddenFromAllWalls,
+        All,
         NotFollowed
     }
 }
