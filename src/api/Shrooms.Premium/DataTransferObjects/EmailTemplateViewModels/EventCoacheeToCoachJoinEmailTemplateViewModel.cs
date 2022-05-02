f49abe86782c524031dabeb06077409eca872beb
INTERNAL\justinas.ruslys <justinas.ruslys@visma.com>
2022-05-02 14:22:13 +0300

    added view models for emails

---
+++ src/api/Shrooms.Premium/DataTransferObjects/EmailTemplateViewModels/EventCoacheeToCoachJoinEmailTemplateViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Shrooms.Contracts.DataTransferObjects;
+
+namespace Shrooms.Premium.DataTransferObjects.EmailTemplateViewModels
+{
+    public class EventCoacheeToCoachJoinEmailTemplateViewModel : BaseEmailTemplateViewModel
+    {
+        public string CoacheeFullName { get; set; }
+
+        public string EventName { get; set; }
+
+        public string EventUrl { get; set; }
+
+        public DateTime EventStartDate { get; set; }
+
+        public DateTime EventEndDate { get; set; }
+
+        public EventCoacheeToCoachJoinEmailTemplateViewModel(
+            string userNotificationSettingsUrl,
+            string coacheeFullName,
+            string eventName,
+            DateTime eventStartDate,
+            DateTime eventEndDate,
+            string eventUrl)
+            : base(userNotificationSettingsUrl)
+        {
+            CoacheeFullName = coacheeFullName;
+            EventName = eventName;
+            EventStartDate = eventStartDate;
+            EventEndDate = eventEndDate;
+            EventUrl = eventUrl;
+        }
+    }
+}
---
+++ src/api/Shrooms.Premium/DataTransferObjects/EmailTemplateViewModels/EventCoacheeToCoachLeaveEmailTemplateViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Shrooms.Contracts.DataTransferObjects;
+
+namespace Shrooms.Premium.DataTransferObjects.EmailTemplateViewModels
+{
+    public class EventCoacheeToCoachLeaveEmailTemplateViewModel : BaseEmailTemplateViewModel
+    {
+        public string CoacheeFullName { get; set; }
+
+        public string EventName { get; set; }
+
+        public string EventUrl { get; set; }
+
+        public EventCoacheeToCoachLeaveEmailTemplateViewModel(
+            string userNotificationSettingsUrl,
+            string coacheeFullName,
+            string eventName,
+            string eventUrl)
+            : base(userNotificationSettingsUrl)
+        {
+            CoacheeFullName = coacheeFullName;
+            EventName = eventName;
+            EventUrl = eventUrl;
+        }
+    }
+}
