08f6efbbbce9cdce5600a6678a64c994dd668631
INTERNAL\justinas.ruslys <justinas.ruslys@visma.com>
2022-05-02 14:15:50 +0300

    added dto for passing data needed for emails

---
+++ src/api/Shrooms.Contracts/DataTransferObjects/Users/UserEventAttendStatusChangeEmailDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Shrooms.Contracts.DataTransferObjects.Users
+{
+    public class UserEventAttendStatusChangeEmailDto
+    {
+        public string FullName => $"{FirstName} {LastName}";
+        
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public int OrganizationId { get; set; }
+
+        public string ManagerId { get; set; }
+
+        public string ManagerEmail { get; set; }
+
+        public string EventName { get; set; }
+
+        public Guid EventId { get; set; }
+
+        public DateTime EventStartDate { get; set; }
+
+        public DateTime EventEndDate { get; set; }
+    }
+}
