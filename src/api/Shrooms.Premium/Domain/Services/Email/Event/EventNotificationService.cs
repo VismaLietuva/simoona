1b09c5c897d08625116f833e766b65f91df9ddfb
INTERNAL\justinas.ruslys <justinas.ruslys@visma.com>
2022-05-02 14:23:48 +0300

    added method for sending email to manager

--- src/api/Shrooms.Premium/Domain/Services/Email/Event/EventNotificationService.cs
+++ src/api/Shrooms.Premium/Domain/Services/Email/Event/EventNotificationService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Shrooms.Contracts.DAL;
 using Shrooms.Contracts.DataTransferObjects;
+using Shrooms.Contracts.DataTransferObjects.Users;
 using Shrooms.Contracts.Infrastructure;
 using Shrooms.Contracts.Infrastructure.Email;
 using Shrooms.DataLayer.EntityModels.Models;
@@ -69,6 +70,55 @@ namespace Shrooms.Premium.Domain.Services.Email.Event
 
             var emailBody = _mailTemplate.Generate(emailTemplateViewModel, EmailPremiumTemplateCacheKeys.EventJoinRemind);
             await _mailingService.SendEmailAsync(new EmailDto(emails, $"Join weekly event now", emailBody));
+        }
+
+        public async Task NotifyManagerAboutEventAsync(UserEventAttendStatusChangeEmailDto userAttendStatusDto, bool isJoiningEvent)
+        {
+            var organization = await _organizationService.GetOrganizationByIdAsync(userAttendStatusDto.OrganizationId);
+            var userNotificationSettingsUrl = _appSettings.UserNotificationSettingsUrl(organization.ShortName);
+            var eventUrl = _appSettings.EventUrl(organization.ShortName, userAttendStatusDto.EventId.ToString());
+
+            var emailDto = GetManagerNotifyEmailDto(userAttendStatusDto, userNotificationSettingsUrl, eventUrl, isJoiningEvent);
+
+            await _mailingService.SendEmailAsync(emailDto);
+        }
+
+        private EmailDto GetManagerNotifyEmailDto(UserEventAttendStatusChangeEmailDto userAttendStatusDto, string userNotificationSettingsUrl, string eventUrl, bool isJoiningEvent)
+        {
+            if(!isJoiningEvent)
+            {
+                var emailTemplateLeaveViewModel = new EventCoacheeToCoachLeaveEmailTemplateViewModel(
+                    userNotificationSettingsUrl,
+                    userAttendStatusDto.FullName,
+                    userAttendStatusDto.EventName,
+                    eventUrl);
+
+                var emailLeaveBody = _mailTemplate.Generate(emailTemplateLeaveViewModel, EmailPremiumTemplateCacheKeys.EventCoacheeToCoachLeaveEmail);
+
+                return new EmailDto(
+                    userAttendStatusDto.FullName,
+                    userAttendStatusDto.Email,
+                    userAttendStatusDto.ManagerEmail,
+                    "Coachee paliko renginį",
+                    emailLeaveBody);
+            }
+
+            var emailTemplateJoinViewModel = new EventCoacheeToCoachJoinEmailTemplateViewModel(
+                userNotificationSettingsUrl,
+                userAttendStatusDto.FullName,
+                userAttendStatusDto.EventName,
+                userAttendStatusDto.EventStartDate,
+                userAttendStatusDto.EventEndDate,
+                eventUrl);
+
+            var emailJoinBody = _mailTemplate.Generate(emailTemplateJoinViewModel, EmailPremiumTemplateCacheKeys.EventCoacheeToCoachJoinEmail);
+
+            return new EmailDto(
+                userAttendStatusDto.FullName,
+                userAttendStatusDto.Email,
+                userAttendStatusDto.ManagerEmail,
+                "Coachee prisijungė prie renginio",
+                emailJoinBody);
         }
     }
 }
--- src/api/Shrooms.Premium/Domain/Services/Email/Event/IEventNotificationService.cs
+++ src/api/Shrooms.Premium/Domain/Services/Email/Event/IEventNotificationService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Shrooms.Contracts.DataTransferObjects.Users;
 using Shrooms.Premium.DataTransferObjects.Models.Events;
 
 namespace Shrooms.Premium.Domain.Services.Email.Event
@@ -10,5 +11,7 @@ namespace Shrooms.Premium.Domain.Services.Email.Event
         Task NotifyRemovedEventParticipantsAsync(string eventName, Guid eventId, int orgId, IEnumerable<string> users);
 
         Task RemindUsersToJoinEventAsync(IEnumerable<EventTypeDto> eventType, IEnumerable<string> users, int orgId);
+
+        Task NotifyManagerAboutEventAsync(UserEventAttendStatusChangeEmailDto userAttendStatusDto, bool isJoiningEvent);
     }
 }
