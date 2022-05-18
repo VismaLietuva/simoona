1cacd0d63e57552d722ffd64e70886946b7ef4e3
INTERNAL\justinas.ruslys <justinas.ruslys@visma.com>
2022-05-18 11:46:16 +0300

    added dtos

---
+++ src/api/Shrooms.Premium/DataTransferObjects/Models/Events/ExtensiveEventParticipantDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shrooms.Premium.DataTransferObjects.Models.Events
+{
+    public class ExtensiveEventParticipantDto
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime? EmploymentDate { get; set; }
+
+        public string QualificationLevel { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public string ManagerFirstName { get; set; }
+
+        public string ManagerLastName { get; set; }
+
+        public string ManagerId { get; set; }
+
+        public decimal? Kudos { get; set; }
+        
+        public IEnumerable<string> PreviouslyAttendedConferences { get; set; }
+
+        public IEnumerable<string> Projects { get; set; }
+    }
+}
