6bc9b7358deba9b9e8a0178bd8f4d3a0aae52783
jusrus01 <jjustinas.ruslys@gmail.com>
2022-05-18 12:04:02 +0300

    added view models

---
+++ src/api/Shrooms.Premium/Presentation/WebViewModels/Events/EventDetailsListItemViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shrooms.Premium.Presentation.WebViewModels.Events
+{
+    public class EventDetailsListItemViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string TypeName { get; set; }
+
+        public IEnumerable<string> OfficeNames { get; set; }
+
+        public int MaxParticipants { get; set; }
+
+        public int ParticipantsCount { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ src/api/Shrooms.Premium/Presentation/WebViewModels/Events/ExtensiveEventDetailsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Shrooms.Premium.DataTransferObjects.Models.Events;
+using System;
+using System.Collections.Generic;
+
+namespace Shrooms.Premium.Presentation.WebViewModels.Events
+{
+    public class ExtensiveEventDetailsViewModel
+    {
+        public string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string[] OfficeNames { get; set; }
+
+        public string HostFirstName { get; set; }
+
+        public string HostLastName { get; set; }
+
+        public string HostUserId { get; set; }
+
+        public string ImageName { get; set; }
+
+        public string Location { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsForAllOffices { get; set; }
+
+        public IEnumerable<ExtensiveEventParticipantDto> ExtensiveParticipants { get; set; }
+    }
+}
