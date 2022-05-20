6909dca7838baf8eef71a015476ffa0fe028f24b
jusrus01 <jjustinas.ruslys@gmail.com>
2022-05-20 10:17:21 +0300

    added dtos

---
+++ src/api/Shrooms.Premium/DataTransferObjects/Models/Events/EventProjectDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Shrooms.Premium.DataTransferObjects.Models.Events
+{
+    public class EventProjectDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
