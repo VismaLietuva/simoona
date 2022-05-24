599cf66d248d462aba4f1c5029f5f78b9fe580f9
jusrus01 <jjustinas.ruslys@gmail.com>
2022-05-24 17:52:36 +0300

    Added expand button with temporary template

---
+++ src/webapp/src/client/app/events/details-list/event-details/expand-button/expand-button.directive.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('simoonaApp.Events')
+        .directive('aceExpandButton', expandButton);
+
+    function expandButton() {
+        var directive = {
+            templateUrl: 'app/events/details-list/event-details/expand-button/expand-button.html',
+            restrict: 'E',
+            replace: true,
+            scope: {
+                isExpanded: '=',
+            },
+            controller: expandButtonController,
+            controllerAs: 'vm',
+            bindToController: true,
+        }
+
+        return directive;
+    }
+
+    function expandButtonController() {
+        var vm = this;
+
+        vm.toggleExpandCollapse = toggleExpandCollapse;
+
+        function toggleExpandCollapse() {
+            vm.isExpanded = !vm.isExpanded;
+        }
+    }
+})();
