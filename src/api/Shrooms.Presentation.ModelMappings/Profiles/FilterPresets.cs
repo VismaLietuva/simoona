75cdedc876482bbab2afdad5d9559814dccd55af
jusrus01 <jjustinas.ruslys@gmail.com>
2022-05-25 13:42:09 +0300

    Added mappings

---
+++ src/api/Shrooms.Presentation.ModelMappings/Profiles/FilterPresets.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Shrooms.Contracts.DataTransferObjects.FilterPresets;
+using Shrooms.Presentation.WebViewModels.Models.FilterPresets;
+
+namespace Shrooms.Presentation.ModelMappings.Profiles
+{
+    public class FilterPresets : Profile
+    {
+        protected override void Configure()
+        {
+            CreateDtoToViewModelMappings();
+            CreateViewModelToDtoMappings();
+        }
+
+        private void CreateDtoToViewModelMappings()
+        {
+        }
+
+        private void CreateViewModelToDtoMappings()
+        {
+            CreateMap<CreateFilterPresetViewModel, CreateFilterPresetDto>();
+            CreateMap<FilterPresetItemViewModel, FilterPresetItemDto>();
+        }
+    }
+}
