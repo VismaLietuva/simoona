85bd7d1ca8abcf060d7cbcb238ac38981d44d4a6
jusrus01 <jjustinas.ruslys@gmail.com>
2022-05-25 13:45:27 +0300

    Added FilterPresetController

---
+++ src/api/Shrooms.Presentation.Api/Controllers/FilterPresetController.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using AutoMapper;
+using Shrooms.Contracts.Constants;
+using Shrooms.Contracts.DataTransferObjects.FilterPresets;
+using Shrooms.Contracts.Exceptions;
+using Shrooms.Domain.Services.FilterPresets;
+using Shrooms.Presentation.Api.Filters;
+using Shrooms.Presentation.WebViewModels.Models.FilterPresets;
+
+namespace Shrooms.Presentation.Api.Controllers
+{
+    [RoutePrefix("FilterPreset")]
+    [Authorize]
+    public class FilterPresetController : BaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IFilterPresetService _filterPresetService;
+
+        public FilterPresetController(IMapper mapper, IFilterPresetService filterPresetService)
+        {
+            _mapper = mapper;
+            _filterPresetService = filterPresetService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [PermissionAuthorize(Permission = AdministrationPermissions.Administration)]
+        public async Task<IHttpActionResult> Create(CreateFilterPresetViewModel createViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var createDto = _mapper.Map<CreateFilterPresetViewModel, CreateFilterPresetDto>(createViewModel);
+
+            try
+            {
+                await _filterPresetService.CreateAsync(createDto);
+             
+                return Ok();
+            }
+            catch (ValidationException e)
+            {
+                return BadRequestWithError(e);
+            }
+        }
+    }
+}
