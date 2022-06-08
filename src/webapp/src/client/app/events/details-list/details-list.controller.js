e9ef161f60f383b0a0d2bf2b220d19b02678c8be
jusrus01 <jjustinas.ruslys@gmail.com>
2022-06-08 10:02:32 +0300

    Added preset logic to event details list

--- src/webapp/src/client/app/events/details-list/details-list.controller.js
+++ src/webapp/src/client/app/events/details-list/details-list.controller.js
@@ -8,17 +8,26 @@
         'notifySrv',
         'eventRepository',
         '$state',
+        'filterPageTypes',
+        'filterTypes',
+        'filterPresetRepository',
+        'filterPresetService',
     ];
 
     function eventDetailsListController(
         notifySrv,
         eventRepository,
-        $state
+        $state,
+        filterPageTypes,
+        filterTypes,
+        filterPresetRepository,
+        filterPresetService
     ) {
         var vm = this;
 
         vm.isLoadingControls = true;
         vm.isLoadingEvents = true;
+        vm.filterPageType = filterPageTypes.extensiveEventDetailsList;
 
         vm.page = 1;
         vm.filter = {
@@ -26,32 +35,54 @@
             appliedOfficeTypes: undefined,
         };
 
+        vm.filterTypes = {
+            eventTypes: undefined,
+            officeTypes: undefined,
+        };
+
+        vm.dropdownCheckboxes = {
+            eventTypes: new Map(),
+            officeTypes: new Map()
+        };
+
         vm.onSearchFilter = onSearchFilter;
         vm.loadEventsOnPage = loadEventsOnPage;
         vm.viewDetails = viewDetails;
         vm.loadEventsWithNewlyAppliedFilter = loadEventsWithNewlyAppliedFilter;
+        vm.applyFilterPreset = applyFilterPreset;
 
         init();
 
-
         function init() {
-            loadEventTypes();
-            loadOfficeTypes();
+            loadFilters();
             loadEvents();
         }
 
-        function loadEventTypes() {
+        function loadFilters() {
             vm.isLoadingControls = true;
 
-            eventRepository.getEventTypes().then(
-                function (result) {
-                    vm.eventTypes = result;
-                    vm.isLoadingControls = false;
-                },
-                function () {
-                    notifySrv.error('errorCodeMessages.messageError');
-                }
-            );
+            filterPresetRepository
+                .getFilters([filterTypes.events, filterTypes.offices])
+                .then(
+                    function (result) {
+                        vm.filterTypes.eventTypes =
+                            filterPresetService.getFiltersByTypeFromResult(
+                                result,
+                                filterTypes.events
+                            );
+
+                        vm.filterTypes.officeTypes =
+                            filterPresetService.getFiltersByTypeFromResult(
+                                result,
+                                filterTypes.offices
+                            );
+
+                        vm.isLoadingControls = false;
+                    },
+                    function () {
+                        notifySrv.error('errorCodeMessages.messageError');
+                    }
+                );
         }
 
         function loadEvents() {
@@ -75,17 +106,42 @@
                 );
         }
 
-        function loadOfficeTypes() {
-            vm.isLoadingControls = true;
+        function applyFilterPreset(preset) {
+            vm.dropdownCheckboxes.eventTypes = mapFilterPresetTypesToMap(
+                preset,
+                filterTypes.events
+            );
 
-            eventRepository.getEventOffices().then(
-                function (result) {
-                    vm.officeTypes = result;
-                    vm.isLoadingControls = false;
-                },
-                function () {
-                    notifySrv.error('errorCodeMessages.messageError');
-                }
+            vm.dropdownCheckboxes.officeTypes = mapFilterPresetTypesToMap(
+                preset,
+                filterTypes.offices
+            );
+
+            applyFilter([...vm.dropdownCheckboxes.eventTypes], filterTypes.events);
+            applyFilter([...vm.dropdownCheckboxes.officeTypes], filterTypes.offices);
+
+            loadEventsOnPage(1);
+        }
+
+        function mapFilterPresetTypesToMap(preset, filterType) {
+            var presetTypes = filterPresetService.getFiltersByTypeFromResult(
+                preset.filters,
+                filterType
+            );
+
+            if (!presetTypes) {
+                return new Map();
+            }
+
+            var filters = Object.values(vm.filterTypes).find(
+                (filter) => filter.filterType == filterType
+            ).filters;
+
+            return new Map(
+                presetTypes.types.map((type) => [
+                    parseInt(type),
+                    filters.find((filter) => filter.id == type).name,
+                ])
             );
         }
 
@@ -103,12 +159,15 @@
             $state.go('Root.WithOrg.Client.Events.Details.Event', { id: id });
         }
 
-        function loadEventsWithNewlyAppliedFilter(filter, filterName) {
+        function applyFilter(filter, filterName) {
             vm.filter = {
                 ...vm.filter,
-                [filterName]: filter.map(f => f[0]),
+                [filterName]: filter.map((f) => f[0]),
             };
+        }
 
+        function loadEventsWithNewlyAppliedFilter(filter, filterName) {
+            applyFilter(filter, filterName);
             loadEventsOnPage(1);
         }
     }
