2c926984f82fe0fae4970ec56bb942e53c2f76a9
jusrus01 <jjustinas.ruslys@gmail.com>
2022-06-29 12:48:38 +0300

    Added FilterPresetController tests

---
+++ src/api/Shrooms.Tests/Controllers/WebApi/FilterPresetControllerTests.cs
@@ -0,0 +1,155 @@
+﻿using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using NUnit.Framework;
+using Shrooms.Contracts.DataTransferObjects.FilterPresets;
+using Shrooms.Contracts.Enums;
+using Shrooms.Contracts.Exceptions;
+using Shrooms.Domain.Services.FilterPresets;
+using Shrooms.Presentation.Api.Controllers;
+using Shrooms.Presentation.WebViewModels.Models.FilterPresets;
+using Shrooms.Tests.ModelMappings;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Hosting;
+
+namespace Shrooms.Tests.Controllers.WebApi
+{
+    [TestFixture]
+    public class FilterPresetControllerTests
+    {
+        private FilterPresetController _filterPresetController;
+
+        private IFilterPresetService _filterPresetService;
+
+        [SetUp]
+        public void TestInitializer()
+        {
+            _filterPresetService = Substitute.For<IFilterPresetService>();
+
+            _filterPresetController = new FilterPresetController(ModelMapper.Create(), _filterPresetService);
+
+            _filterPresetController.ControllerContext = Substitute.For<HttpControllerContext>();
+            _filterPresetController.Request = new HttpRequestMessage();
+            _filterPresetController.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+            _filterPresetController.Request.SetConfiguration(new HttpConfiguration());
+            _filterPresetController.RequestContext.Principal =
+                new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.NameIdentifier, "1"), new Claim("OrganizationId", "1") }));
+        }
+
+        [Test]
+        public async Task Update_Should_Return_Ok()
+        {
+            // Arrange
+            _filterPresetService.UpdateAsync(Arg.Any<AddEditDeleteFilterPresetDto>())
+                .Returns(new UpdatedFilterPresetDto());
+
+            var updateViewModel = new AddEditDeleteFilterPresetViewModel();
+
+            // Act
+            var httpActionResult = await _filterPresetController.Update(updateViewModel);
+            var response = await httpActionResult.ExecuteAsync(CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Test]
+        public async Task Update_Should_Return_BadRequest_When_ValidationException_Is_Thrown()
+        {
+            // Arrange
+            _filterPresetService.UpdateAsync(Arg.Any<AddEditDeleteFilterPresetDto>())
+                .Throws(new ValidationException(0));
+
+            var updateViewModel = new AddEditDeleteFilterPresetViewModel();
+
+            // Act
+            var httpActionResult = await _filterPresetController.Update(updateViewModel);
+            var response = await httpActionResult.ExecuteAsync(CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Test]
+        public async Task GetPresets_Should_Return_Ok()
+        {
+            // Arrange
+            _filterPresetService.GetPresetsForPageAsync(Arg.Any<PageType>(), Arg.Any<int>())
+                .Returns(Enumerable.Empty<FilterPresetDto>());
+
+            var pageType = PageType.EventDetailsList;
+
+            // Act
+            var httpActionResult = await _filterPresetController.GetPresets(pageType);
+            var response = await httpActionResult.ExecuteAsync(CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Test]
+        public async Task GetPresets_Should_Return_BadRequest_When_ValidationException_Is_Thrown()
+        {
+            // Arrange
+            _filterPresetService.GetPresetsForPageAsync(Arg.Any<PageType>(), Arg.Any<int>())
+                .Throws(new ValidationException(0));
+
+            var pageType = PageType.EventDetailsList;
+            
+            // Act
+            var httpActionResult = await _filterPresetController.GetPresets(pageType);
+            var response = await httpActionResult.ExecuteAsync(CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Test]
+        public async Task GetFilters_Should_Return_Ok()
+        {
+            // Arrange
+            _filterPresetService.GetFiltersAsync(Arg.Any<FilterType[]>(), Arg.Any<int>())
+                .Returns(Enumerable.Empty<FiltersDto>());
+            
+            var filterTypes = new[]
+            {
+                FilterType.Kudos,
+                FilterType.Events
+            };
+
+            // Act
+            var httpActionResult = await _filterPresetController.GetFilters(filterTypes);
+            var response = await httpActionResult.ExecuteAsync(CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Test]
+        public async Task GetFilters_Should_Return_BadRequest_When_ValidationException_Is_Thrown()
+        {
+            // Arrange
+            _filterPresetService.GetFiltersAsync(Arg.Any<FilterType[]>(), Arg.Any<int>())
+                .Throws(new ValidationException(0));
+
+            var filterTypes = new[]
+            {
+                FilterType.Kudos,
+                FilterType.Events
+            };
+
+            // Act
+            var httpActionResult = await _filterPresetController.GetFilters(filterTypes);
+            var response = await httpActionResult.ExecuteAsync(CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
