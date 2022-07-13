91f6297f3171976dbe5939ca3140c2651709f67f
jusrus01 <jjustinas.ruslys@gmail.com>
2022-07-13 15:07:30 +0300

    Added dto and view model for visited events

---
+++ src/api/Shrooms.Premium/DataTransferObjects/Models/Events/EventParticipantVisitedEventsListingArgsDto.cs
@@ -0,0 +1,23 @@
+﻿using Shrooms.Contracts.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace Shrooms.Premium.DataTransferObjects.Models.Events
+{
+    public class EventParticipantVisitedEventsListingArgsDto : IPageable, ISortable, IFilterableByDate
+    {
+        public string UserId { get; set; }
+
+        public int Page { get; set; }
+
+        public int PageSize { get; set; }
+
+        public string SortByProperties { get; set; }
+
+        public IEnumerable<int> EventTypeIds { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+    }
+}
---
+++ src/api/Shrooms.Premium/Presentation/WebViewModels/Events/EventParticipantVisitedEventsListingArgsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Shrooms.Contracts.Infrastructure;
+using Shrooms.Premium.Constants;
+using Shrooms.Premium.Presentation.WebViewModels.ValidationAttributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shrooms.Premium.Presentation.WebViewModels.Events
+{
+    public class EventParticipantVisitedEventsListingArgsViewModel : IPageable, ISortable, IFilterableByDate
+    {
+        public string UserId { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Page { get; set; } = 1;
+
+        [Range(1, int.MaxValue)]
+        public int PageSize { get; set; } = EventsConstants.EventsDefaultPageSize;
+        
+        public string SortByProperties { get; set; }
+
+        public IEnumerable<int> EventTypeIds { get; set; }
+
+        [Required]
+        [DateTimeLessThanDateTime("EndDate")]
+        public DateTime? StartDate { get; set; }
+        
+        [Required]
+        public DateTime? EndDate { get; set; }
+    }
+}
