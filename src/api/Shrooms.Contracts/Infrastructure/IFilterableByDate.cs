2dd4b671988bd22386ac4286e739424f7839a357
jusrus01 <jjustinas.ruslys@gmail.com>
2022-07-13 15:04:47 +0300

    Created interface for filtering by date

---
+++ src/api/Shrooms.Contracts/Infrastructure/IFilterableByDate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Shrooms.Contracts.Infrastructure
+{
+    public interface IFilterableByDate
+    {
+        DateTime? StartDate { get; set; }
+
+        DateTime? EndDate { get; set; }
+    }
+}
