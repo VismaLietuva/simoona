1077822218729900eadd1ab8a7bd89fc27337752
jusrus01 <jjustinas.ruslys@gmail.com>
2022-07-27 13:06:35 +0300

    Added base abstract class that all style classes should implement

---
+++ src/api/Shrooms.Infrastructure/ExcelGenerator/Styles/ExcelBuilderStyleBase.cs
@@ -0,0 +1,9 @@
+﻿using OfficeOpenXml;
+
+namespace Shrooms.Infrastructure.ExcelGenerator.Styles
+{
+    public abstract class ExcelBuilderStyleBase
+    {
+        public abstract void Apply(ExcelWorksheet worksheet);
+    }
+}
