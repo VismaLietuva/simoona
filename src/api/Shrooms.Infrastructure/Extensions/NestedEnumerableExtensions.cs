db3fcef9bdf1c482b4d13bf5cb39cea4c9d31c7a
jusrus01 <jjustinas.ruslys@gmail.com>
2022-07-27 13:07:02 +0300

    Added an extension method for looping through all cells

---
+++ src/api/Shrooms.Infrastructure/Extensions/NestedEnumerableExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Shrooms.Infrastructure.Extensions
+{
+    public static class NestedEnumerableExtensions
+    {
+        /// <summary>
+        /// Loops through doubly nested enumerable
+        /// </summary>
+        /// <returns>Row index and column index</returns>
+        public static IEnumerable<(int, int)> GetRowsPositionEnumerator<T>(this IEnumerable<IEnumerable<T>> enumerable)
+        {
+            var currentRowIndex = -1;
+
+            foreach (var row in enumerable)
+            {
+                currentRowIndex++;
+
+                var currentColumnIndex = 0;
+
+                foreach (var column in row)
+                {
+                    yield return (currentRowIndex, currentColumnIndex++);
+                }
+            }
+        }
+    }
+}
