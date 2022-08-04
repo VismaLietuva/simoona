d959039075a1689bf0939daaf2adb1650394d01e
jusrus01 <jjustinas.ruslys@gmail.com>
2022-08-04 10:46:27 +0300

    Renamed and added validation attributes

--- src/api/Shrooms.Premium/DataTransferObjects/Models/Lotteries/BuyLotteryTicketsDto.cs
+++ src/api/Shrooms.Premium/DataTransferObjects/Models/Lotteries/BuyLotteryTicketsDto.cs
@@ -1,9 +1,11 @@
 ﻿namespace Shrooms.Premium.DataTransferObjects.Models.Lotteries
 {
-    public class BuyLotteryTicketDto
+    public class BuyLotteryTicketsDto
     {
         public int LotteryId { get; set; }
 
         public int Tickets { get; set; }
+
+        public string[] ReceivingUserIds { get; set; }
     }
 }
---
+++ src/api/Shrooms.Premium/Presentation/WebViewModels/Lotteries/BuyLotteryTicketsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shrooms.Premium.Presentation.WebViewModels.Lotteries
+{
+    public class BuyLotteryTicketsViewModel
+    {
+        [Required]
+        public int? LotteryId { get; set; }
+
+        [Required]
+        public int? Tickets { get; set; }
+
+        [MinLength(1)]
+        public string[] ReceivingUserIds { get; set; }
+    }
+}
