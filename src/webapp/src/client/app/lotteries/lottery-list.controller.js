734e139e1cc1d3df23f3a23c92730ab6d519ed21
jusrus01 <jjustinas.ruslys@gmail.com>
2022-08-16 16:10:09 +0300

    Updated frontend to send correct data

--- src/webapp/src/client/app/lotteries/lottery-list.controller.js
+++ src/webapp/src/client/app/lotteries/lottery-list.controller.js
@@ -1,26 +1,32 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('simoonaApp.Lotteries')
         .constant('editableLotteries', ['drafted', 'started'])
         .constant('lotteryPageSettings', {
-            'pageSize': 10
+            pageSize: 10,
         })
         .controller('lotteryListController', lotteryListController);
 
     lotteryListController.$inject = [
         '$rootScope',
         'authService',
-        '$location',
         'lotteryRepository',
         'lotteryStatuses',
         'lotteryPageSettings',
-        'editableLotteries'
-    ];    
+        'editableLotteries',
+    ];
 
-    function lotteryListController($rootScope, authService, $location, lotteryRepository, lotteryStatuses, lotteryPageSettings, editableLotteries) {
-    	/* jshint validthis: true */
+    function lotteryListController(
+        $rootScope,
+        authService,
+        lotteryRepository,
+        lotteryStatuses,
+        lotteryPageSettings,
+        editableLotteries
+    ) {
+        /* jshint validthis: true */
         var vm = this;
         vm.lotteryStatuses = lotteryStatuses;
         vm.editableLotteries = editableLotteries;
@@ -30,22 +36,25 @@
         vm.getLotteryStatusString = getLotteryStatusString;
         vm.isLotteryEditable = isLotteryEditable;
         $rootScope.pageTitle = 'lotteries.lotteriesPanelHeader';
-        vm.allowEdit = authService.hasPermissions(["LOTTERY_ADMINISTRATION"]);
+        vm.allowEdit = authService.hasPermissions(['LOTTERY_ADMINISTRATION']);
         vm.showRefundPage = showRefundPage;
+        vm.onSort = onSort;
 
         init();
 
         function init() {
-            lotteryRepository.getLotteryListPaged(vm.filters).then(function (response) {
-                vm.lotteries = response.pagedList;
-                vm.filters.itemCount = response.itemCount;
-            })
+            lotteryRepository
+                .getLotteryListPaged(vm.filters)
+                .then(function (response) {
+                    vm.lotteries = response.pagedList;
+                    vm.filters.itemCount = response.itemCount;
+                });
         }
 
         function onSearch(searchString) {
-            vm.filters.searchString = searchString;
-            vm.filters.page = 1;
-            changeState();
+            onCompleteLoadFirstPage(function() {
+                vm.filters.searchString = searchString;
+            });
         }
 
         function onPageChange() {
@@ -53,30 +62,54 @@
         }
 
         function changeState() {
-            var filterParams = {};
+            var filterParams = { sortByProperties: vm.filters.sortString };
+
             if (!!vm.filters.page) {
                 filterParams.page = vm.filters.page;
             }
+
             if (!!vm.filters.searchString) {
                 filterParams.filter = vm.filters.searchString;
             }
-            lotteryRepository.getLotteryListPaged(filterParams).then(function (lotteries) {
-                vm.lotteries = lotteries.pagedList;
-                vm.filters.itemCount = lotteries.itemCount;
-            });
+
+            lotteryRepository
+                .getLotteryListPaged(filterParams)
+                .then(function (lotteries) {
+                    vm.lotteries = lotteries.pagedList;
+                    vm.filters.itemCount = lotteries.itemCount;
+                });
         }
 
         function getLotteryStatusString(status) {
-            return 'lotteries.' + Object.keys(vm.lotteryStatuses).find(key => vm.lotteryStatuses[key] === status);
+            return (
+                'lotteries.' +
+                Object.keys(vm.lotteryStatuses).find(
+                    (key) => vm.lotteryStatuses[key] === status
+                )
+            );
         }
 
         function isLotteryEditable(lottery) {
-            return vm.editableLotteries.some(status => vm.lotteryStatuses[status] === lottery.status);
+            return vm.editableLotteries.some(
+                (status) => vm.lotteryStatuses[status] === lottery.status
+            );
         }
 
         function showRefundPage(lottery) {
             return lottery.refundFailed;
         }
-    }
 
+        function onSort(sortValue, sortDirection) {
+            onCompleteLoadFirstPage(function() {
+                vm.filters.sortString = `${sortValue} ${sortDirection}`;
+            });
+        }
+
+        function onCompleteLoadFirstPage(func) {
+            func();
+
+            vm.filters.page = 1;
+            changeState();
+        }
+    }
 })();
