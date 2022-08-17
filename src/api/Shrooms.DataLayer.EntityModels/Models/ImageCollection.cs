d8d75a2b9dbd7313079544a12af8358630e3f4a9
jusrus01 <jjustinas.ruslys@gmail.com>
2022-08-17 13:58:40 +0300

    Moved ImageCollection and added constructors

--- src/api/Shrooms.DataLayer.EntityModels/Models/ImageCollection.cs
+++ src/api/Shrooms.DataLayer.EntityModels/Models/ImageCollection.cs
@@ -6,10 +6,29 @@ namespace Shrooms.DataLayer.EntityModels.Models
 {
     public class ImageCollection : Collection<string>
     {
+        public ImageCollection()
+        {
+        }
+
+        public ImageCollection(IEnumerable<string> images)
+        {
+            AddImages(images);
+        }
+
         public void Add(ICollection<string> images)
+        {
+            AddImages(images);
+        }
+
+        private void AddImages(IEnumerable<string> images)
         {
             foreach (var image in images)
             {
+                if (string.IsNullOrEmpty(image))
+                {
+                    continue;
+                }
+
                 Add(image);
             }
         }
