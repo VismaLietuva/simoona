eb42765553129da9d855feff3394882fe85e3eac
jusrus01 <jjustinas.ruslys@gmail.com>
2022-08-18 14:51:58 +0300

    Added comment view model tests

---
+++ src/api/Shrooms.Tests/Controllers/ViewModels/Comments/NewCommentViewModelTests.cs
@@ -0,0 +1,117 @@
+﻿using NUnit.Framework;
+using Shrooms.Contracts.Constants;
+using Shrooms.Presentation.WebViewModels.Models.Wall.Posts.Comments;
+using Shrooms.Tests.Extensions;
+using System.Collections.Generic;
+
+namespace Shrooms.Tests.Controllers.ViewModels.Comments
+{
+    [TestFixture]
+    public class NewCommentViewModelTests
+    {
+        [Test]
+        public void NewInstance_ValidValues_ReturnsTrue()
+        {
+            // Arrange
+            var viewModel = CreateValidViewModel();
+
+            // Assert
+            Assert.IsTrue(viewModel.IsValid());
+        }
+
+        [Test]
+        public void NewInstance_InvalidPostIdValue_ReturnsFalse()
+        {
+            // Arrange
+            var viewModel = CreateValidViewModel();
+
+            viewModel.PostId = 0;
+
+            // Assert
+            Assert.IsFalse(viewModel.IsValid());
+        }
+
+        [Test]
+        public void NewInstance_WithoutMessageBodyAndWithImages_ReturnsTrue()
+        {
+            // Arrange
+            var viewModel = CreateValidViewModel();
+
+            viewModel.MessageBody = null;
+
+            // Assert
+            Assert.IsTrue(viewModel.IsValid());
+        }
+
+        [Test]
+        public void NewInstance_WithoutMessageBodyAndWithoutImages_ReturnsFalse()
+        {
+            // Arrange
+            var viewModel = CreateValidViewModel();
+
+            viewModel.Images = null;
+            viewModel.MessageBody = null;
+
+            // Assert
+            Assert.IsFalse(viewModel.IsValid());
+        }
+
+        [Test]
+        public void NewInstance_WithoutMessageBodyAndEmptyImageCollection_ReturnsFalse()
+        {
+            // Arrange
+            var viewModel = CreateValidViewModel();
+
+            viewModel.Images = new List<string>();
+            viewModel.MessageBody = null;
+
+            // Assert
+            Assert.IsFalse(viewModel.IsValid());
+        }
+
+        [Test]
+        public void NewInstance_WithoutMessageBodyAndImagesContainNullValues_ReturnsFalse()
+        {
+            // Arrange
+            var viewModel = CreateValidViewModel();
+
+            viewModel.Images = new List<string>
+            {
+                "image",
+                null,
+                "image"
+            };
+
+            viewModel.MessageBody = null;
+
+            // Assert
+            Assert.IsFalse(viewModel.IsValid());
+        }
+
+        [Test]
+        public void NewInstance_MessageBodyOverCharacterLimit_ReturnsFalse()
+        {
+            // Arrange
+            var viewModel = CreateValidViewModel();
+
+            viewModel.MessageBody = new string('-', ValidationConstants.MaxCommentMessageBodyLength + 1);
+
+            // Assert
+            Assert.IsFalse(viewModel.IsValid());
+        }
+
+        private NewCommentViewModel CreateValidViewModel()
+        {
+            return new NewCommentViewModel
+            {
+                MessageBody = "body",
+                PostId = 1,
+                Images = new List<string>
+                {
+                    "image"
+                },
+                MentionedUserIds = new List<string>()
+            };
+        }
+    }
+}
