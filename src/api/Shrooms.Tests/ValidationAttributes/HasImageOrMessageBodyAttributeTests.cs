2a6e9dc647378e41b7e1272bccaf804ac9276fe5
jusrus01 <jjustinas.ruslys@gmail.com>
2022-08-18 15:53:36 +0300

    Added attribute tests

---
+++ src/api/Shrooms.Tests/ValidationAttributes/HasImageOrMessageBodyAttributeTests.cs
@@ -0,0 +1,151 @@
+﻿using NUnit.Framework;
+using Shrooms.Presentation.WebViewModels.ValidationAttributes.Walls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shrooms.Tests.ValidationAttributes
+{
+    [TestFixture]
+    public class HasImageOrMessageBodyAttributeTests
+    {
+        private class Mock
+        {
+            public int Number { get; set; }
+
+            public string MessageBody { get; set; }
+        }
+
+        [Test]
+        public void IsValid_MessageBodyIsNotString_ThrowsArgumentException()
+        {
+            // Arrange
+            var model = new Mock();
+            var validationContext = new ValidationContext(model);
+            string testValue = null;
+
+            var attribute = new HasImageOrMessageBodyAttribute(nameof(Mock.Number));
+
+            // Assert
+            Assert.Throws<ArgumentException>(() => attribute.GetValidationResult(testValue, validationContext));
+        }
+
+        [Test]
+        public void IsValid_ImagesIsNotIEnumerable_ThrowsArgumentException()
+        {
+            // Arrange
+            var model = new Mock();
+            var validationContext = new ValidationContext(model);
+            var testValue = string.Empty;
+
+            var attribute = new HasImageOrMessageBodyAttribute(nameof(Mock.MessageBody));
+
+            // Assert
+            Assert.Throws<ArgumentException>(() => attribute.GetValidationResult(testValue, validationContext));
+        }
+
+        [Test]
+        public void IsValid_MessageBodyIsNotEmptyAndImagesIsNull_ReturnsTrue()
+        {
+            // Arrange
+            var model = new Mock();
+
+            model.MessageBody = "not empty";
+
+            var validationContext = new ValidationContext(model);
+            IEnumerable<string> testValue = null;
+
+            var attribute = new HasImageOrMessageBodyAttribute(nameof(Mock.MessageBody));
+
+            // Act
+            var actual = attribute.GetValidationResult(testValue, validationContext);
+
+            // Assert
+            Assert.AreEqual(ValidationResult.Success, actual);
+        }
+
+        [Test]
+        public void IsValid_MessageBodyIsNullAndImagesContainNullValues_ReturnsFalse()
+        {
+            // Arrange
+            var model = new Mock();
+            var validationContext = new ValidationContext(model);
+            var testValue = new List<string>
+            {
+                "image",
+                null,
+                "image"
+            };
+
+            var attribute = new HasImageOrMessageBodyAttribute(nameof(Mock.MessageBody));
+
+            // Act
+            var actual = attribute.GetValidationResult(testValue, validationContext);
+
+            // Assert
+            Assert.IsNotNull(actual.ErrorMessage);
+        }
+
+        [Test]
+        public void IsValid_MessageBodyIsNullAndImagesPropertyIsEmpty_ReturnsFalse()
+        {
+            // Arrange
+            var model = new Mock();
+            var validationContext = new ValidationContext(model);
+            var testValue = new List<string>();
+
+            var attribute = new HasImageOrMessageBodyAttribute(nameof(Mock.MessageBody));
+
+            // Act
+            var actual = attribute.GetValidationResult(testValue, validationContext);
+
+            // Assert
+            Assert.IsNotNull(actual.ErrorMessage);
+        }
+
+        [Test]
+        public void IsValid_MessageBodyIsNullAndThereAreImages_ReturnsTrue()
+        {
+            // Arrange
+            var model = new Mock();
+            var validationContext = new ValidationContext(model);
+            var testValue = new List<string>
+            {
+                "image",
+                "image"
+            };
+
+            var attribute = new HasImageOrMessageBodyAttribute(nameof(Mock.MessageBody));
+
+            // Act
+            var actual = attribute.GetValidationResult(testValue, validationContext);
+
+            // Assert
+            Assert.AreEqual(ValidationResult.Success, actual);
+        }
+
+        [Test]
+        public void IsValid_MessageBodyIsNotNullAndThereAreImages_ReturnsTrue()
+        {
+            // Arrange
+            var model = new Mock();
+
+            model.MessageBody = "not empty";
+
+            var validationContext = new ValidationContext(model);
+            var testValue = new List<string>
+            {
+                "image",
+                "image"
+            };
+
+            var attribute = new HasImageOrMessageBodyAttribute(nameof(Mock.MessageBody));
+
+            // Act
+            var actual = attribute.GetValidationResult(testValue, validationContext);
+
+            // Assert
+            Assert.AreEqual(ValidationResult.Success, actual);
+        }
+    }
+}
