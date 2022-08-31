65f4260a99a9468bb2b622cae8493049fb0a58aa
jusrus01 <jjustinas.ruslys@gmail.com>
2022-08-31 12:10:57 +0300

    Removed everything related to Monitors

--- src/api/Shrooms.Contracts.DataTransferObjects/Models/Monitors/MonitorDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Shrooms.Contracts.DataTransferObjects.Models.Monitors
-{
-    public class MonitorDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- src/api/Shrooms.DataLayer.EntityModels/Models/Monitors/Monitor.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Shrooms.DataLayer.EntityModels.Models.Monitors
-{
-    public class Monitor : BaseModelWithOrg
-    {
-        public string Name { get; set; }
-    }
-}
