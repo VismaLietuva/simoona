24ed7d2a25aadc72e996d8ab005b18259ea55f50
jusrus01 <jjustinas.ruslys@gmail.com>
2022-09-01 09:42:05 +0300

    Added email compiler tests

---
+++ src/api/Shrooms.Tests/Infrastructure/EmailTemplateCompilerTests.cs
@@ -0,0 +1,32 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Shrooms.Contracts.Infrastructure.Email;
+using Shrooms.Infrastructure.Email;
+using System;
+
+namespace Shrooms.Tests.Infrastructure
+{
+    [TestFixture]
+    public class EmailTemplateCompilerTests
+    {
+        private IMailTemplateCache _mailTemplateCache;
+        private string _baseDirectory;
+
+        private EmailTemplateCompiler _sut;
+
+        [SetUp]
+        public void TestInitializer()
+        {
+            _mailTemplateCache = Substitute.For<IMailTemplateCache>();
+            _baseDirectory = AppDomain.CurrentDomain.BaseDirectory + @"\..\..\..\Shrooms.Presentation.Api";
+
+            _sut = new EmailTemplateCompiler(_mailTemplateCache, _baseDirectory);
+        }
+
+        [Test]
+        public void Register_WhenCompiling_DoesNotThrow()
+        {
+            Assert.DoesNotThrow(() => _sut.Register());
+        }
+    }
+}
