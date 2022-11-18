e15212f7e7c5ab191d1ffe80d83ed46cc99bc62b
Kristupas Lunskas <kristupas.lunskas@visma.com>
2022-11-18 13:13:36 +0200

    add temporary countdown to game of thrones

--- src/webapp/src/client/app/wall/wall.controller.js
+++ src/webapp/src/client/app/wall/wall.controller.js
@@ -14,6 +14,36 @@
         /*jshint validthis: true */
         var vm = this;
 
+                //TEMPORARY COUNTER
+                const second = 1000,
+                minute = second * 60,
+                hour = minute * 60,
+                day = hour * 24;
+        
+                //var gotStarts = new Date("Dec 9, 2022 18:00:00").getTime();
+                var gotStarts = new Date("Nov 18, 2022 13:10:20").getTime();
+                
+                const countDown = new Date(gotStarts).getTime();
+                const x = setInterval(function() {    
+        
+                        const now = new Date().getTime(),
+                            distance = countDown - now;
+        
+                        if (distance < 0) {
+                            document.getElementById("heading").innerText = "Game of Thrones starts now!";
+                            document.getElementById("counting-sm").style.visibility = document.getElementById("counting-lg").style.visibility = 'hidden';
+                            
+                            clearInterval(x);
+                        } else {
+                            document.getElementById("days-sm").innerText = document.getElementById("days-lg").innerText = Math.floor(distance / (day)),
+                            document.getElementById("hours-sm").innerText = document.getElementById("hours-lg").innerText = Math.floor((distance % (day)) / (hour)),
+                            document.getElementById("minutes-sm").innerText = document.getElementById("minutes-lg").innerText = Math.floor((distance % (hour)) / (minute)),
+                            document.getElementById("seconds-sm").innerText = document.getElementById("seconds-lg").innerText = Math.floor((distance % (minute)) / second);
+                        }
+                        //seconds
+                    }, 0)
+                //TEMPORARY COUNTER
+
         $rootScope.pageTitle = 'wall.wallTitle';
 
         //init
