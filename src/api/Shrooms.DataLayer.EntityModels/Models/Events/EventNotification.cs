ec4ab7977c48dbce45e56262f46406f6f57f6e94
jusrus01 <jjustinas.ruslys@gmail.com>
2022-12-30 10:36:12 +0200

    Add EventNotification entity

---
+++ src/api/Shrooms.DataLayer.EntityModels/Models/Events/EventNotification.cs
@@ -0,0 +1,24 @@
+﻿using Shrooms.Contracts.DataTransferObjects;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shrooms.DataLayer.EntityModels.Models.Events
+{
+    public class EventNotification : ISoftDelete
+    {
+        [Key]
+        [ForeignKey(nameof(Event))]
+        public Guid EventId { get; set; }
+
+        public Event Event { get; set; }
+
+        public int RemindBeforeEventStartInDays { get; set; }
+
+        public bool EventStartNotified { get; set; }
+
+        public int RemindBeforeEventRegistrationDeadlineInDays { get; set; }
+
+        public bool EventRegistrationDeadlineNotified { get; set; }
+    }
+}
