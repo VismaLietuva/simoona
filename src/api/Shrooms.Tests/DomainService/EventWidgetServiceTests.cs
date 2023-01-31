91b9a772d8027cc0382da36cb090788e5410b310
jusrus01 <jjustinas.ruslys@gmail.com>
2023-01-31 13:39:31 +0200

    Add event service widget tests

---
+++ src/api/Shrooms.Tests/DomainService/EventWidgetServiceTests.cs
@@ -0,0 +1,168 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Shrooms.Contracts.DAL;
+using Shrooms.DataLayer.EntityModels.Models.Events;
+using System.Data.Entity;
+using Shrooms.Tests.Extensions;
+using Shrooms.Domain.Services.Events;
+using Shrooms.Contracts.Infrastructure;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Shrooms.Contracts.DataTransferObjects.Models.Events;
+
+namespace Shrooms.Tests.DomainService
+{
+    [TestFixture]
+    public class EventWidgetServiceTests
+    {
+        private const int DefaultOrganizationId = 1;
+
+        private DbSet<Event> _eventsDbSet;
+        private ISystemClock _systemClock;
+        
+        private EventWidgetService _sut;//TODO: Add tests that checks bool
+
+        [SetUp]
+        public void TestInitializer()
+        {
+            var uow = Substitute.For<IUnitOfWork2>();
+            _systemClock = Substitute.For<ISystemClock>();
+            _eventsDbSet = uow.MockDbSetForAsync<Event>();
+
+            _sut = new EventWidgetService(uow, _systemClock);
+        }
+
+        [Test]
+        public void GetUpcomingEventsAsync_WhenEventCountIsLessThanOne_Throws()
+        {
+            // Arrange
+            const int eventCount = -1;
+
+            // Assert
+            Assert.ThrowsAsync<ArgumentException>(async () => await _sut.GetUpcomingEventsAsync(DefaultOrganizationId, eventCount));
+        }
+
+        [Test]
+        public async Task GetUpcomingEventsAsync_WhenEventCountIsFive_ReturnsFiveEvents()
+        {
+            // Arrange
+            const int eventCount = 5;
+            var currentDate = DateTime.UtcNow;
+            CreateEventsForGetUpcomingEventsAsyncTest(
+                currentDate,
+                currentDate,
+                eventsWithUpcomingTypeSet: eventCount,
+                eventCount: eventCount);
+            _systemClock.UtcNow.Returns(currentDate.AddDays(-10));
+
+            // Act
+            var result = await _sut.GetUpcomingEventsAsync(DefaultOrganizationId, eventCount);
+
+            // Assert
+            Assert.AreEqual(eventCount, result.Count());
+        }
+
+        [Test]
+        public async Task GetUpcomingEventsAsync_WhenAllEventsAreAlreadyStarted_ReturnsEmptyCollection()
+        {
+            // Arrange
+            const int eventCount = 5;
+            var currentDate = DateTime.UtcNow;
+            CreateEventsForGetUpcomingEventsAsyncTest(currentDate, currentDate, eventsWithUpcomingTypeSet: eventCount, eventCount: eventCount);
+            _systemClock.UtcNow.Returns(currentDate.AddDays(10));
+
+            // Act
+            var result = await _sut.GetUpcomingEventsAsync(DefaultOrganizationId, eventCount);
+
+            // Assert
+            Assert.IsFalse(result.Any());
+        }
+
+        [Test]
+        public async Task GetUpcomingEventsAsync_WhenEventsAreNotStarted_ReturnsEventsOrderedInDescendingOrderByDeadlineDateThenByStartDate()
+        {
+            // Arrange
+            const int eventCount = 5;
+            var currentDate = DateTime.UtcNow;
+            var eventStartDate = currentDate;
+            var eventDeadlineDate = currentDate.AddDays(-20);
+            CreateEventsForGetUpcomingEventsAsyncTest(
+                eventStartDate,
+                eventDeadlineDate,
+                eventsWithUpcomingTypeSet: eventCount,
+                eventCount: eventCount,
+                additionalEvents: () => new List<Event>
+                {
+                    new Event
+                    {
+                        Id = Guid.NewGuid(),
+                        OrganizationId = DefaultOrganizationId,
+                        StartDate = currentDate.AddDays(-50),
+                        RegistrationDeadline = currentDate.AddDays(-60),
+                        EventType = new EventType
+                        {
+                            Name = Guid.NewGuid().ToString(),
+                            IsShownInUpcomingEvents = true
+                        }
+                    }
+                });
+            _systemClock.UtcNow.Returns(currentDate.AddDays(-30));
+
+            // Act
+            var result = await _sut.GetUpcomingEventsAsync(DefaultOrganizationId, eventCount);
+
+            // Assert
+            CollectionAssert.AreEqual(result.OrderByDescending(x => x.RegistrationDeadlineDate).ThenByDescending(x => x.StartDate), result);
+        }
+
+        [Test]
+        public async Task GetUpcomingEventsAsync_WhenEventsAreNotStartedAndTypeDoesNotAllowToShowEvent_ReturnsEventsThatCanBeShown()
+        {
+            // Arrange
+            const int eventsWithUpcomingTypeSet = 3;
+            var currentDate = DateTime.UtcNow;
+            CreateEventsForGetUpcomingEventsAsyncTest(currentDate, currentDate, eventsWithUpcomingTypeSet);
+            _systemClock.UtcNow.Returns(currentDate.AddDays(-10));
+
+            // Act
+            var result = await _sut.GetUpcomingEventsAsync(DefaultOrganizationId);
+
+            // Assert
+            Assert.AreEqual(eventsWithUpcomingTypeSet, result.Count());
+        }
+
+        public void CreateEventsForGetUpcomingEventsAsyncTest(
+            DateTime eventStartDate,
+            DateTime eventDeadlineDate,
+            int eventsWithUpcomingTypeSet = 3,
+            int eventCount = 5,
+            Func<List<Event>> additionalEvents = null)
+        {
+            var events = new List<Event>();
+            for (var i = 0; i < eventCount; i++)
+            {
+                events.Add(new Event
+                {
+                    Id = Guid.NewGuid(),
+                    OrganizationId = DefaultOrganizationId,
+                    StartDate = eventStartDate,
+                    RegistrationDeadline = eventDeadlineDate,
+                    EventType = new EventType
+                    {
+                        Name = Guid.NewGuid().ToString(),
+                        IsShownInUpcomingEvents = i < eventsWithUpcomingTypeSet
+                    }
+                });
+            }
+
+            if (additionalEvents != null)
+            {
+                events.AddRange(additionalEvents());
+            }
+
+            _eventsDbSet.SetDbSetDataForAsync(events);
+        }
+    }
+}
