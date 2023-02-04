d3bfb1d88608be4e42a44e8b694b6dcb65f83b79
jusrus01 <jjustinas.ruslys@gmail.com>
2023-02-04 17:08:53 +0200

    Add app notification when event is shared

--- src/api/Shrooms.Premium/Presentation/Api/BackgroundWorkers/SharedEventNotifier.cs
+++ src/api/Shrooms.Premium/Presentation/Api/BackgroundWorkers/SharedEventNotifier.cs
@@ -1,8 +1,11 @@
 ﻿using System.Threading.Tasks;
+using AutoMapper;
 using Shrooms.Contracts.DataTransferObjects;
 using Shrooms.Contracts.DataTransferObjects.Wall.Posts;
 using Shrooms.Contracts.Infrastructure;
+using Shrooms.Contracts.ViewModels.Notifications;
 using Shrooms.Domain.Services.Email.Posting;
+using Shrooms.Domain.Services.Notifications;
 using Shrooms.Domain.Services.Wall;
 using Shrooms.Presentation.Api.Hubs;
 
@@ -10,21 +13,33 @@ namespace Shrooms.Premium.Presentation.Api.BackgroundWorkers
 {
     public class SharedEventNotifier : IBackgroundWorker
     {
+        private readonly IMapper _mapper;
         private readonly IWallService _wallService;
         private readonly IPostNotificationService _postNotificationService;
+        private readonly INotificationService _notificationService;
 
-        public SharedEventNotifier(IWallService wallService, IPostNotificationService postNotificationService)
+        public SharedEventNotifier(
+            IMapper mapper,
+            IWallService wallService,
+            IPostNotificationService postNotificationService,
+            INotificationService notificationService)
         {
+            _mapper = mapper;
             _wallService = wallService;
             _postNotificationService = postNotificationService;
+            _notificationService = notificationService;
         }
 
         public async Task NotifyAsync(NewPostDto postModel, NewlyCreatedPostDto createdPost, UserAndOrganizationHubDto userHubDto)
         {
             await _postNotificationService.NotifyAboutNewPostAsync(createdPost);
 
-            var membersToNotify = await _wallService.GetWallMembersIdsAsync(postModel.WallId, postModel);
-            await NotificationHub.SendWallNotificationAsync(postModel.WallId, membersToNotify, createdPost.WallType, userHubDto);
+            var wallMemberIds = await _wallService.GetWallMembersIdsAsync(postModel.WallId, postModel);
+            var notificationDto = await _notificationService.CreateForPostAsync(userHubDto, createdPost, createdPost.WallId, wallMemberIds);
+            var notificationViewModel = _mapper.Map<NotificationViewModel>(notificationDto);
+
+            await NotificationHub.SendNotificationToParticularUsersAsync(notificationViewModel, userHubDto, wallMemberIds);
+            await NotificationHub.SendWallNotificationAsync(postModel.WallId, wallMemberIds, createdPost.WallType, userHubDto);
         }
     }
 }
