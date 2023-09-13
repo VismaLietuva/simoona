91b0e747c232e86ddc92cc09fd2aca1bbf0ee22f
Evaldas Naujikas <evaldas.naujikas@visma.com>
2023-09-13 15:39:12 +0300

    Update Shrooms.Authentication project

--- src/api/Shrooms.Authentification/Properties/AssemblyInfo.cs
+++ src/api/Shrooms.Authentification/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Shrooms.Authentification")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Shrooms.Authentification")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 
 [assembly: Guid("da661aa0-3313-4c2d-81a9-c1a2b74f7317")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
