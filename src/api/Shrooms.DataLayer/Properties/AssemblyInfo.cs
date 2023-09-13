c0e8daa193b2cec6c39aee5e001cf6aedf031cd6
Evaldas Naujikas <evaldas.naujikas@visma.com>
2023-09-13 16:27:17 +0300

    Update Shrooms.DataLayer

--- src/api/Shrooms.DataLayer/Properties/AssemblyInfo.cs
+++ src/api/Shrooms.DataLayer/Properties/AssemblyInfo.cs
@@ -1,18 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Shrooms.DataLayer")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Shrooms.DataLayer")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("a42ef5ab-c74f-4c40-b210-1de2c2f8fcc6")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
