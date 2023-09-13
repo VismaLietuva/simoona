bef84b879776f82e386c7fe423f21c65fd840f48
Evaldas Naujikas <evaldas.naujikas@visma.com>
2023-09-13 15:42:01 +0300

    Update Shrooms.Contracts

--- src/api/Shrooms.Contracts/Properties/AssemblyInfo.cs
+++ src/api/Shrooms.Contracts/Properties/AssemblyInfo.cs
@@ -1,18 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Shrooms.Contracts")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Shrooms.Contracts")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("e690ceb8-f266-441f-8a80-5a3440b3445a")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
