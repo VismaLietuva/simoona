078d4536de955d9042b2b1f56d7b0b83590fc203
Evaldas Naujikas <evaldas.naujikas@visma.com>
2023-09-13 15:45:06 +0300

    Update Shrooms.Domain

--- src/api/Shrooms.Domain/Properties/AssemblyInfo.cs
+++ src/api/Shrooms.Domain/Properties/AssemblyInfo.cs
@@ -1,18 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Shrooms.Domain")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Shrooms.Domain")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("ce6b9a61-361a-4e30-bd30-61b2463e8bc5")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
