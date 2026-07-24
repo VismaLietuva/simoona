d10639cac01ecc4255350cf299bdcc33dfc17b07
Edgaras Legus <edgaras.legus@visma.com>
2026-07-24 16:36:35 +0300

    fix - removed the converter

--- src/api/Shrooms.Presentation.WebViewModels/Models/Users/Kudos/KudosLogTypeViewModel.cs
+++ src/api/Shrooms.Presentation.WebViewModels/Models/Users/Kudos/KudosLogTypeViewModel.cs
@@ -1,5 +1,3 @@
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 using Shrooms.Contracts.Enums;
 
 namespace Shrooms.Presentation.WebViewModels.Models.Users.Kudos
@@ -12,7 +10,6 @@ namespace Shrooms.Presentation.WebViewModels.Models.Users.Kudos
 
         public decimal Value { get; set; }
 
-        [JsonConverter(typeof(StringEnumConverter))]
         public KudosTypeEnum Type { get; set; }
     }
 }
